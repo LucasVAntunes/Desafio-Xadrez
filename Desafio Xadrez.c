696cd2f553ce6ca74b050bd2e91843ebb83fa22f
LucasVAntunes <lucasvieira001@gmail.com>
2025-03-03 19:55:21 +0000

    Desfio basico versao final

--- Desafio Xadrez.c
+++ Desafio Xadrez.c
@@ -2,39 +2,79 @@
 
 int main() {
     
-//Declaração das variáveis das peças e da escolha da peça a ser movimentada
+    //Declaração variável de escolha da peça a ser movimentada
 
-    int torre, bispo, rainha, escolha;
+    int escolha;
 
-    printf ("Seja bem vindo enxadrista!\n\n");
+    printf ("Seja bem vindo enxadrista!\n");
 
     //Estrutura de repetição para fechar o programa somente em caso de escolha do usuário    
+    
     do {
+    
+    //Declaração variáveis de incremento das peças
 
-    printf ("Digite o número referente a peça a ser movimentada:\n\n1. Torre\n2. Bispo\n3. Rainha\n0. Sair\n\n");
+    int torre = 0;
+    int bispo = 0;
+    int rainha;
+    
+    //Menu de escolha
+
+    printf ("\nDigite o número referente a peça a ser movimentada:\n\n1. Torre\n2. Bispo\n3. Rainha\n0. Sair\n\n");
     scanf ("%d", &escolha);
 
     switch (escolha)
     {
+
+    //Movimento Torre
+
     case 1:
 
         printf ("\nVocê escolheu a Torre!\n\n");
+        
+        do {
+            
+            printf("Direita\n");
+            torre++;
+
+        } while (torre < 5);
         break;
     
+    //Movimento Bispo
+
     case 2:
     
         printf ("\nVocê escolheu o Bispo!\n\n");
+        
+        while (bispo < 5)
+        {
+            printf ("Cima\nDireita\n");
+            bispo++;
+        }
+        
         break;
     
+    //Movimento Rainha
+
     case 3:
         
         printf ("\nVocê escolheu a Rainha!\n\n");
+
+        for (rainha = 0; rainha < 8; rainha++)
+        {
+            printf ("Esquerda\n");
+        }
+        
         break;
+
+    //Opção de saída do programa
 
     case 0:
 
-        printf ("Saindo do programa...\n\n");
+        printf ("\nSaindo do programa...\n\n");
         break;
+
+    //Opção inválida
 
     default:
 
